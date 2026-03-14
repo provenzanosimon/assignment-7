622c38e28033b0f0cf48ad4c0e1e81cbeb8156b8
provenzanosimon <provesj@millsaps.edu>
2026-03-14 17:18:35 -0500

    Created favoriteFoods array and loop

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 console.log("Hello World!");
 // 1. Create an array called favoriteFoods with at least 6 foods you love.
-
+let favoriteFoods = ["Ramen", "Pizza", "Sushi", "Tacos", "Burgers", "Pasta"];
 
 
 // 2. Loop through the list and print: "One of my favorite foods is ______."
-
+for (let i = 0; i < favoriteFoods.length; i++) {
+  console.log("One of my favorite foods is " + favoriteFoods[i] + ".");
+}
 
 
 
